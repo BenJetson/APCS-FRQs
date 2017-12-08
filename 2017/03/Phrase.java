94da7b0a82626998ff4b216e5ea7cf98afbfefa8
Ben Godfrey <BenJetson@users.noreply.github.com>
2017-12-08 10:14:47 -0500

    mostly working

---
+++ 2017/03/Phrase.java
@@ -0,0 +1,63 @@
+public class Phrase {
+
+    private String currentPhrase;
+
+    public Phrase(String p) {
+        this.currentPhrase = p;
+    }
+
+    public int findNthOccurrence(String str, int n) {
+
+        int index = 0;
+        int counter = 0;
+
+        do {
+            
+            index = currentPhrase.indexOf(str, index);
+
+            if (index == -1) {
+                return -1;
+            }
+
+            counter++;
+            index += str.length();
+
+        } while(counter < n);
+
+        return index;
+    }
+
+    public void replaceNthOccurrence(String str, int n, String repl) {
+        int nthIndex = findNthOccurrence(str, n);
+        System.out.println(nthIndex);
+        if (nthIndex != -1) {
+            String prefix = currentPhrase.substring(0, nthIndex);
+            String suffix = currentPhrase.substring(nthIndex + str.length(), currentPhrase.length());
+            currentPhrase = prefix + repl + suffix;
+        }
+    }
+
+    public int findLastOccurrence(String str) {
+        int index = -1; 
+        int counter = 1;
+        boolean foundLast = false;
+
+        do {
+            int currentIndex = findNthOccurrence(str, counter);
+
+            if (currentIndex == -1 || counter > 10) {
+                foundLast = true;
+            } else {
+                counter++;
+                index = currentIndex;
+            }
+
+        } while (!foundLast);
+
+        return index;
+    }
+
+    public String toString() {
+        return currentPhrase;
+    }
+}
---
+++ 2017/03/PhraseTester.java
@@ -0,0 +1,33 @@
+public class PhraseTester {
+
+    public static void main(String[] args) {
+
+        Phrase phrase1 = new Phrase("A cat ate late.");
+        phrase1.replaceNthOccurrence("at", 1, "rane");
+        System.out.println(phrase1);
+
+        Phrase phrase2 = new Phrase("A cat ate late."); 
+        phrase2.replaceNthOccurrence("at", 6, "xx"); 
+        System.out.println(phrase2); 
+
+        Phrase phrase3 = new Phrase("A cat ate late."); 
+        phrase3.replaceNthOccurrence("bat", 2, "xx"); 
+        System.out.println(phrase3); 
+
+        Phrase phrase4 = new Phrase("aaaa"); 
+        phrase4.replaceNthOccurrence("aa", 1, "xx"); 
+        System.out.println(phrase4);
+
+        Phrase phrase5 = new Phrase("aaaa"); 
+        phrase5.replaceNthOccurrence("aa", 2, "bbb"); 
+        System.out.println(phrase5); 
+        System.out.println(phrase5.findNthOccurrence("a", 45));
+
+        Phrase phrase6 = new Phrase("A cat ate late.");
+        System.out.println(phrase6.findNthOccurrence("cat", 1));
+        System.out.println(phrase6.findLastOccurrence("at"));
+        System.out.println(phrase6.findLastOccurrence("cat")); 
+        System.out.println(phrase6.findLastOccurrence("bat"));
+    }
+
+}
