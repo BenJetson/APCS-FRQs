613986539780101264fd9e4f2dc58b4081a7bf4d
Ben Godfrey <BenJetson@users.noreply.github.com>
2018-02-08 10:06:41 -0500

    javadoc'd 2007-03

--- 2007/03/StudentAnswerSheet.java
+++ 2007/03/StudentAnswerSheet.java
@@ -1,15 +1,39 @@
 import java.util.ArrayList;
 
+/**
+ * Class that provides useful methods for grading and 
+ * manipulating student MC test answers.
+ * 
+ * @author Ben Godfrey
+ * @version 08 FEB 2018
+ */
 public class StudentAnswerSheet {
 
+    /** The student's name. */
     private String name;
+    /** The student's answers. */
     private ArrayList<String> answers;
 
+    /** 
+     * Constructs a new StudentAnswerSheet given required info.
+     * 
+     * @param name The student's name.
+     * @param answers The student's answers. 
+     */
     public StudentAnswerSheet(String name, ArrayList<String> answers) {
         this.name = name;
         this.answers = answers; 
     }
 
+    /**
+     * Scores the answer sheet given the answer key. Each
+     *   correct answer gets one point, each incorrect answer
+     *   takes away 1/4 of a point, and blank answers are ingored.
+     * 
+     * @param key The answer key for the test.
+     * 
+     * @return The student's score.
+     */
     public double getScore(ArrayList<String> key) {
         int numCorrect = 0;
         int numWrong   = 0;
@@ -26,11 +50,21 @@ public class StudentAnswerSheet {
 
         return numCorrect - (0.25 * numWrong);
     }
-    
+
+    /**
+     * Gets the student's name. 
+     * 
+     * @return The student's name.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Returns a String representation of this object.
+     * 
+     * @return A string representation of this object.
+     */
     public String toString() {
         return String.format("---\nScoresheet\nName: %s\nAnswers: %s\n---\n", 
                               name, answers.toString());
--- 2007/03/TestResults.java
+++ 2007/03/TestResults.java
@@ -1,13 +1,35 @@
 import java.util.ArrayList;
 
+/**
+ * Provides useful methods for manipulating a group
+ *   of StudentAnswerSheets for a particular test.
+ * 
+ * @author Ben Godfrey 
+ * @version 08 FEB 2018
+ */
 public class TestResults {
 
+    /** The list of sheets. */
     private ArrayList<StudentAnswerSheet> sheets;
 
+    /** 
+     * Creates a new TestResults object given the answer sheets.
+     * 
+     * @param sheets The answer sheets from the test.
+     */
     public TestResults(ArrayList<StudentAnswerSheet> sheets) {
         this.sheets = sheets;
     }
 
+    /**
+     * Finds the highest scoring student out of a group of
+     *   answer sheets. In the event of a tie, only the name
+     *   of the first sheet in the stack with the high score 
+     *   is returned.
+     * 
+     * @param key The answer key for the test.
+     * @return The name of the highest scoring student.
+     */
     public String highestScoringStudent(ArrayList<String> key) {
         double highScore = key.size() * -1;
         String name = "";
