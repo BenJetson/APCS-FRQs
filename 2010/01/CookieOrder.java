3663fcefdbc3666076c1a36df0c3bc4f9733782e
Ben Godfrey <BenJetson@users.noreply.github.com>
2018-02-14 09:47:10 -0500

    javadoc'd cookie order

--- 2010/01/CookieOrder.java
+++ 2010/01/CookieOrder.java
@@ -1,21 +1,50 @@
+/**
+ * Helper class that provides structure for a cookie order.
+ * 
+ * @author Ben Godfrey
+ * @version 13 FEB 2018
+ */
 public class CookieOrder {
 
+    /** The variety of cookies in the order. */
     private String variety;
+    /** The number of boxes ordered. */
     private int numBoxes;
 
+    /**
+     * Creates a new CookieOrder object.
+     * 
+     * @param variety The variety of cookies to order.
+     * @param numBoxes The number of boxes to order.
+     */
     public CookieOrder(String variety, int numBoxes) {
         this.variety = variety;
         this.numBoxes = numBoxes;
     }
 
+    /**
+     * Returns the variety of cookies ordered.
+     * 
+     * @return The variety of cookies ordered.
+     */
     public String getVariety() {
         return variety;
     }
 
+    /**
+     * Returns the number of boxes ordered.
+     * 
+     * @return The number of boxes ordered.
+     */
     public int getNumBoxes() {
         return numBoxes;
     }
 
+    /**
+     * Returns a representation of this object as a string. 
+     * 
+     * @return String representation of this object. 
+     */
     public String toString() {
         return String.format("{ CookieOrder - variety: %s,  numBoxes: %d }",
                                 variety, numBoxes);
--- 2010/01/MasterOrder.java
+++ 2010/01/MasterOrder.java
@@ -1,18 +1,36 @@
 import java.util.List;
 import java.util.ArrayList;
 
+/**
+ * Provides a structure for handling multiple orders of cookies.
+ * 
+ * @author Ben Godfrey
+ * @version 13 FEB 2018
+ */
 public class MasterOrder {
 
+    /** Holds the list of cookie orders. */
     private List<CookieOrder> orders;
 
+    /** Constructs a new master order. */
     public MasterOrder() {
         orders = new ArrayList<CookieOrder>();
     }
 
+    /**
+     * Adds another order of cookies to the master order.
+     * 
+     * @param theOrder The order to be added.
+     */
     public void addOrder(CookieOrder theOrder) {
         orders.add(theOrder);
     }
 
+    /**
+     * Calculates the total number of boxes in the master order.
+     * 
+     * @return The total number of boxes.
+     */
     public int getTotalBoxes() {
         int sum = 0;
 
@@ -23,6 +41,14 @@ public class MasterOrder {
         return sum;
     }
 
+    /**
+     * Removes all orders for a particular variety of cookies
+     *   from the master order. 
+     * 
+     * @param cookieVar The variety of cookies to be removed.
+     * 
+     * @return The number of boxes removed.
+     */
     public int removeVariety(String cookieVar) {
         int counter = 0;
 
@@ -40,6 +66,11 @@ public class MasterOrder {
         return counter;
     }
 
+    /**
+     * Returns a representation of this object as a string. 
+     * 
+     * @return String representation of this object. 
+     */
     public String toString() {
         String output = "---MASTER ORDER---\n";
 
